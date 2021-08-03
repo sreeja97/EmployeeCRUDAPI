4f219140c31b1283b7b1f3f89a1fa25bf287cd24
sreeja <govardhanasrija@gmail.com>
2021-08-03 19:57:21 +0530

    Creating CRUD api for Employee App ussing MongoDB

---
+++ EmployeeApp/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from EmployeeApp.models import Employees,Departments
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Departments 
+        fields=('DepartmentId','DepartmentName')
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Employees 
+        fields=('EmployeeId','EmployeeName','Department','DateOfJoining','PhotoFileName')
